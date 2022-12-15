7c40db8ef1650ca670b2cacb5b098b090849bfa9
ilyaboo <108785038+ilyaboo@users.noreply.github.com>
2022-12-15 17:55:08 -0500

    Delete twitch_project/target directory

--- twitch_project/target/debug/deps/twitch_project-1dea1ff4f63a9e1f.d
+++
@@ -1,9 +0,0 @@
-/Users/ilya/Desktop/BU courses/DS210/twitch_project/target/debug/deps/twitch_project-1dea1ff4f63a9e1f.rmeta: src/main.rs src/resources/mod.rs src/resources/graph.rs src/resources/matrix.rs src/resources/network.rs
-
-/Users/ilya/Desktop/BU courses/DS210/twitch_project/target/debug/deps/twitch_project-1dea1ff4f63a9e1f.d: src/main.rs src/resources/mod.rs src/resources/graph.rs src/resources/matrix.rs src/resources/network.rs
-
-src/main.rs:
-src/resources/mod.rs:
-src/resources/graph.rs:
-src/resources/matrix.rs:
-src/resources/network.rs:
